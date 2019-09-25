fe7ce8a13ad52d73c9ea8300d288e1f039a3d981
giantwu <wujikong@126.com>
2019-09-25 19:58:58 +0800

    Update HttpClient.php
    头信息accept中增加 ,text/plain

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -82,7 +82,7 @@ class HttpClient
      */
     protected $header = [
         "user-agent"      => 'EasySwooleHttpClient/0.1',
-        'accept'          => 'text/html,application/xhtml+xml,application/xml,application/json',
+        'accept'          => 'text/html,application/xhtml+xml,application/xml,application/json,text/plain',
         'accept-encoding' => 'gzip',
         'pragma'          => 'no-cache',
         'cache-control'   => 'no-cache'
